514cd05492f4ee06568e97c86c6e46bf2f4f4358
Davi Alves Ramos <davi.info@gmail.com>
2021-05-02 18:42:07 -0300

    Versão 2

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ import pickle
 import os
 
 colunas = ['tamanho','ano','garagem']
-# VM GCP  #modelo = pickle.load(open('../../models/modelo.sav','rb'))
-# Docker
 modelo = pickle.load(open('models/modelo.sav','rb'))
 
 app = Flask(__name__)
@@ -36,4 +34,5 @@ def cotacao():
     preco = modelo.predict([dados_input])
     return jsonify(preco=preco[0])
 
-app.run(debug=True, host='0.0.0.0',port='5002') 
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1',port='5002') 
